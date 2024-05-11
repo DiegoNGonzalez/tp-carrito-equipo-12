6f7ba08769967043016143281cc8cd66fe89a6ee
Diegonzalez Dev <diegonzalezdev@gmail.com>
2024-05-11 14:35:44 -0300

    agregando masterpage, default y comenzando con grilla

---
+++ Carrito/default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Carrito.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Carrito.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <asp:GridView runat="server" ID="dgvArticulos" CssClass="table table-primary" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="CodigoArticulo" HeaderText="Codigo" />
+                <asp:BoundField DataField="NombreArticulo" HeaderText="Nombre" />
+                <asp:BoundField DataField="PrecioArticulo" HeaderText="Precio" />
+                <asp:BoundField DataField="MarcaArticulo" HeaderText="Marca" />
+                <asp:BoundField DataField="CategoriaArticulo" HeaderText="Categoria" />
+                </Columns>
+        </asp:GridView>
+
+    </div>
+</asp:Content>
---
+++ Carrito/default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using dominio;
+using AccesoDataBase;
+using Negocio;
+
+namespace Carrito
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private List<Articulo> articulos = new List<Articulo>();
+        private ArticuloNegocio negocio = new ArticuloNegocio();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            negocio = new ArticuloNegocio();
+            dgvArticulos.DataSource = negocio.ListarArticulos();
+            dgvArticulos.DataBind();
+        }
+
+        private void CargarGrid()
+        {
+            try
+            {
+                articulos = negocio.ListarArticulos();
+                if (articulos.Count == 0)
+                {
+                    dgvArticulos.DataSource = null;
+                    //pBoxArticulosFormArticulos.Load("https://i0.wp.com/static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg?ssl=1");
+                }
+                else
+                {
+                    dgvArticulos.DataSource = articulos;
+                    //dgvArticulos.Columns["IdArticulo"].Visible = false;
+                    //dgvArticulos.Columns["DescripcionArticulo"].Visible = false;
+                    //dgvArticulos.Columns["CategoriaArticulo"].Visible = false;
+                    //dgvArticulos.Columns["PrecioArticulo"].DataGridView.Columns["PrecioArticulo"].DefaultCellStyle.Format = "$0.00";
+                    //pBoxArticulosFormArticulos.Load(listaArticulos[0].Imagenes[0].URLImagen);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
