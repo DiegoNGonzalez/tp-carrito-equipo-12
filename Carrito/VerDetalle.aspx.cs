c3d1ef0d3a77cb3388fd79bdbbf80ab437bdbeb7
Sebas Scollo <sebas.scollo24@gmail.com>
2024-05-16 22:49:03 -0300

    Agrega la funcionalidad de añadir articulos al carrito desde la ventana VerDetalles.aspx

--- Carrito/VerDetalle.aspx.cs
+++ Carrito/VerDetalle.aspx.cs
@@ -8,10 +8,16 @@ using dominio;
 using AccesoDataBase;
 using Negocio;
 
+
+
 namespace Carrito
 {
     public partial class VerDetalle : System.Web.UI.Page
     {
+        public List<Articulo> articulos = new List<Articulo>();
+        public List<ArticuloEnCarrito> articulosEnCarrito = new List<ArticuloEnCarrito>();
+        private ArticuloNegocio negocio = new ArticuloNegocio();
+        public int contadorArticulos = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -32,14 +38,74 @@ namespace Carrito
                     }
                     else
                     {
-                        imgArticulo.ImageUrl = "~/Images/imagen_por_defecto.jpg"; 
+                        imgArticulo.ImageUrl = "~/Images/imagen_por_defecto.jpg";
                     }
                 }
             }
         }
-                    protected void btnAgregar_Click(object sender, EventArgs e)
-                    {
-                        //contadorCarrito.Text = (Convert.ToInt32(contadorCarrito.Text) + 1).ToString();
-                    }
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //contadorCarrito.Text = (Convert.ToInt32(contadorCarrito.Text) + 1).ToString
+            string idArticulo = Request.QueryString["idArticulo"];
+            Session.Add("idArticuloAgregar", idArticulo);
+            AgregarAlCarrito();
+            contadorArticulos = Session["ContadorArticulos"] != null ? Convert.ToInt32(Session["ContadorArticulos"]) + 1 : 1;
+            Session.Add("ContadorArticulos", contadorArticulos);
+            Response.Redirect("default.aspx", false);
+        }
+
+        public void AgregarAlCarrito()
+        {      
+            if (Session["articulosEnCarrito"] != null)
+            {
+                articulosEnCarrito = (List<ArticuloEnCarrito>)Session["articulosEnCarrito"];
+                articulos = (List<Articulo>)Session["articulos"];
+                int id = Convert.ToInt32(Session["idArticuloAgregar"]);
+                Articulo auxArticuloId = articulos.Find(x => x.IDArticulo == id);
+                bool existe = ExisteEnCarrito(id);
+                if (existe)
+                {
+                    ArticuloEnCarrito auxArticuloEnCarrito = articulosEnCarrito.Find(x => x.IDArticulo == id);
+                    auxArticuloEnCarrito.Cantidad++;
+                    auxArticuloEnCarrito.Subtotal = auxArticuloEnCarrito.Cantidad * auxArticuloEnCarrito.PrecioArticulo;
+                    Session.Add("articulosEnCarrito", articulosEnCarrito);
+                    return;
+                }
+                else
+                {
+                    ArticuloEnCarrito articuloEnCarrito = new ArticuloEnCarrito(auxArticuloId);
+                    articulosEnCarrito.Add(articuloEnCarrito);
+                    Session.Add("articulosEnCarrito", articulosEnCarrito);
+                }
+
+
+
+            }
+            else
+            {
+                articulos = (List<Articulo>)Session["articulos"];
+                int id = Convert.ToInt32(Session["idArticuloAgregar"]);
+                Articulo auxArticuloId = articulos.Find(x => x.IDArticulo == id);
+
+                ArticuloEnCarrito articuloEnCarrito = new ArticuloEnCarrito(auxArticuloId);
+                articulosEnCarrito.Add(articuloEnCarrito);
+                Session.Add("articulosEnCarrito", articulosEnCarrito);
+            }
+
+
+        }
+        public bool ExisteEnCarrito(int id)
+        {
+            if (Session["articulosEnCarrito"] != null)
+            {
+                articulosEnCarrito = (List<ArticuloEnCarrito>)Session["articulosEnCarrito"];
+                if (articulosEnCarrito.Find(x => x.IDArticulo == id) != null)
+                {
+                    return true;
                 }
             }
+            return false;
+        }
+
+    }
+}
